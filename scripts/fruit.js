a1445480ccf5da1f5f62258856a48792e4f36859
Bryan Schoot <bdjschoot@gmail.com>
2020-11-26 14:26:34 +0100

    Stage 1

--- scripts/fruit.js
+++ scripts/fruit.js
@@ -1,31 +1,7 @@
 class Fruit {
-    constructor(ctx, scale, rows, columns) {
-        this.ctx = ctx;
-        this.scale = scale;
-        this.rows = rows;
-        this.columns = columns;
-        this.location = this.randomLocation();
-    }
+    constructor() {}
 
-    draw() {
-        this.ctx.fillStyle = "#FF0000";
-        this.ctx.fillRect(this.location.x, this.location.y, this.scale, this.scale);
-        this.ctx.strokeRect(this.location.x, this.location.y, this.scale, this.scale);
-    }
+    draw() {}
 
-    update(snake) {
-        let tmp = null;
-
-        // Checks whether the new location of the fruit is not on the snakes body.
-        while (tmp == null || snake.onSnake(tmp)) {
-            tmp = this.randomLocation();
-        }
-
-        this.location = tmp;
-    }
-
-    randomLocation() {
-        // return random location based on amount of columns and rows.
-        return { x: (Math.floor(Math.random() * this.columns - 1) + 1) * this.scale, y: (Math.floor(Math.random() * this.rows - 1) + 1) * this.scale }
-    }
+    update() {}
 }
--- scripts/game.js
+++ scripts/game.js
@@ -1,59 +1,9 @@
 class Game {
-    constructor() {
-        this.amount = document.getElementById("amount");
-        this.speed = document.getElementById("speed");
-        this.score = document.getElementById("score");
-        this.highscore = document.getElementById("highscore");
-        this.canvas = document.getElementById("canvas");
-        this.ctx = this.canvas.getContext("2d");
-        this.scale = 30;
-        this.rows = this.canvas.height / this.scale;
-        this.columns = this.canvas.width / this.scale;
-        this.gameScore = 0;
-        this.highScore = 0;
-    }
+    constructor() {}
 
-    start() {
-        this.snake = new Snake(this.canvas, this.ctx, this.scale);
-        this.fruit = new Fruit(this.ctx, this.scale, this.rows, this.columns);
+    start() {}
 
-        let interval = setInterval(() => {
-            if (this.snake.alive) {
-                this.update();
-                this.draw();
-            } else {
-                this.endGame();
-                clearInterval(interval);
-            }
-        }, parseInt(this.speed.value));
-    }
+    update() {}
 
-    update() {
-        this.snake.update();
-
-        if (this.snake.eat(this.fruit.location)) {
-            this.gameScore = this.snake.segments.length;
-            this.score.innerText = "Score: " + this.gameScore;
-
-            this.fruit.update(this.snake);
-            this.snake.addSegment(this.amount.value);
-        }
-    }
-
-    draw() {
-        this.ctx.clearRect(0, 0, this.canvas.height, this.canvas.width);
-
-        this.snake.draw();
-        this.fruit.draw();
-    }
-
-    endGame() {
-        alert("You died, hit start to play again!");
-        if (this.gameScore > this.highScore) {
-            this.highScore = this.gameScore;
-            this.highscore.innerText = "High Score: " + this.highScore;
-        }
-        this.gameScore = 0;
-        this.score.innerText = "Score: " + this.gameScore;
-    }
+    draw() {}
 }
